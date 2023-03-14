9b450b35314b58ca401a222bea25853a55e6058d
unknown <dracospy369@gmail.com>
2023-03-14 14:34:54 -0400

    update

--- level_editor.py
+++ level_editor.py
@@ -1,13 +1,14 @@
 import json
 import pygame
 import sys
+from projectgame3 import MazeGenerator
 
 WIDTH, HEIGHT = 1600, 900
 
 pygame.init()
 
 class MenuButton:
-    def __init__(self, menu, pos, width, height, text, functionToCall):
+    def __init__(self, menu, pos, width, height, text, functionToCall, colors = [(200, 200, 200), (255, 255, 255)]):
         self.menu = menu
         self.pos = pos; self.posx, self.posy = pos
         self.width = width
@@ -18,7 +19,9 @@ class MenuButton:
 
         self.button_txt = self.menu.font.render(self.text, False, 'black')
 
-        self.current_color = (200, 200, 200)
+        self.colors = colors
+
+        self.current_color = colors[0]
 
         self.mouse_over = False
 
@@ -33,10 +36,10 @@ class MenuButton:
     def change_bright(self, mouseOver): #true/false
         if mouseOver:
             self.mouse_over = True
-            self.current_color = (255, 255, 255)
+            self.current_color = self.colors[1]
         else:
             self.mouse_over = False
-            self.current_color = (200, 200, 200)
+            self.current_color = self.colors[0]
         self.draw_button()
 
     def update(self):
@@ -70,12 +73,22 @@ class Map:
         self.Xoffset = 0
         self.Yoffset = 0
 
+        self.zoom = 20
+        self.thickness = 2
+
         self.lvlDict = {}
+
+        self.selected = 1
+
+        self.maze_generator = MazeGenerator()
+
+    def change_select(self, val):
+        self.selected = val
 
     def add_width(self):
         self.width += 1
 
-        for y in range(self.height):
+        for y, r in enumerate(self.map):
             self.map[y].append(0)
 
     def add_height(self):
@@ -90,6 +103,43 @@ class Map:
             json.dump(self.lvlDict, fp)
 
         print("Map saved")
+
+    def load_random(self):
+        gen = self.maze_generator.generate_maze(6, 6, 0)
+
+        self.map = gen[0]
+
+        self.Xoffset = 0; self.Yoffset = 0
+
+        self.width = len(self.map[0])
+        self.height = len(self.map)
+
+    def add_walls(self):
+        maze = self.map
+
+        n = 0
+        for r in maze:
+            n_r = [1]
+            for s in r:
+                n_r.append(s)
+            n_r.append(1)
+            maze[n] = n_r
+            n+=1
+
+        top_down_padding = []
+
+        for x in range(self.width + 2):
+            top_down_padding.append(1)
+
+        final_maze = [top_down_padding]
+
+        for r in maze:
+            final_maze.append(r)
+        final_maze.append(top_down_padding)
+
+        self.map = final_maze
+        self.width += 2
+        self.height += 2
 
     def load_game(self):
         with open('result.json') as json_file:
@@ -107,27 +157,42 @@ class Map:
         self.draw_map()
 
     def draw_map(self):
+        def pos_color(val):
+            if val == 0:
+                return 'darkgray'
+            elif val == 1:
+                return 'yellow'
+            elif val == "p":
+                return 'purple'
+
         map_draw = {}
 
         for j, row in enumerate(self.map):
             for i, value in enumerate(row):
                 map_draw[(i, j)] = value
 
-        [pygame.draw.rect(self.editor.screen, ('darkgray' if map_draw[pos] == 0 else 'yellow'), (pos[0] * 20 + self.Xoffset * 20 + 300, pos[1] * 20 + self.Yoffset * 20 + 300, 20, 20), 2) for pos in map_draw]
+        [pygame.draw.rect(self.editor.screen, pos_color(map_draw[pos]), (pos[0] * self.zoom + self.Xoffset * self.zoom + 300, pos[1] * self.zoom + self.Yoffset * self.zoom + 300, self.zoom, self.zoom), self.thickness) for pos in map_draw]
 
     def mouseClick(self, mx, my):
-        row = (my - 300 - self.Yoffset * 20) // 20
-        col = (mx - 300 - self.Xoffset * 20) // 20
+        row = int((my - 300 - self.Yoffset * self.zoom) // self.zoom)
+        col = int((mx - 300 - self.Xoffset * self.zoom) // self.zoom)
 
         if (0 <= col <= self.width-1) and (0 <= row <= self.height-1):
-            if self.map[row][col] == 0:
-                self.map[row][col] = 1
-            else:
+            if self.map[row][col] == self.selected:
                 self.map[row][col] = 0
+            else:
+                self.map[row][col] = self.selected
 
-class ButtonFunctions:
+class ValueSelector:
     def __init__(self, editor):
         self.editor = editor
+        self.map = self.editor.map
+
+    def portal(self):
+        self.map.change_select("p")
+
+    def wall(self):
+        self.map.change_select(1)
 
 class MainEditor:
     def __init__(self):
@@ -136,10 +201,10 @@ class MainEditor:
 
         self.screen = pygame.display.set_mode((WIDTH, HEIGHT))
 
-        self.buttonFuncs = ButtonFunctions(self)
         self.map = Map(self)
 
-        self.buttons = []
+        self.buttons = [] #general buttons
+        self.sbuttons = [] #wall select buttons
 
         self.movekeys = {
             pygame.K_w: False,
@@ -148,10 +213,18 @@ class MainEditor:
             pygame.K_d: False
         }
 
+        self.value_selector = ValueSelector(self)
+
         self.buttons.append(MenuButton(self, (20, 20), 125, 55, "Add Width", self.map.add_width))
         self.buttons.append(MenuButton(self, (150, 20), 125, 55, "Add Height", self.map.add_height))
-        self.buttons.append(MenuButton(self, (1495, 20), 100, 55, "Save", self.map.save_game))
-        self.buttons.append(MenuButton(self, (1495, 80), 100, 55, "Load", self.map.load_game))
+        self.buttons.append(MenuButton(self, (280, 20), 125, 55, "Add Walls", self.map.add_walls))
+
+        self.buttons.append(MenuButton(self, (1490, 20), 100, 55, "Random", self.map.load_random))
+        self.buttons.append(MenuButton(self, (1490, 80), 100, 55, "Save", self.map.save_game))
+        self.buttons.append(MenuButton(self, (1490, 140), 100, 55, "Load", self.map.load_game))
+
+        self.sbuttons.append(MenuButton(self, (500, 20), 50, 50, "P", self.value_selector.portal, colors=[(65, 19, 60), (109, 26, 100)]))
+        self.sbuttons.append(MenuButton(self, (555, 20), 50, 50, "W", self.value_selector.wall, colors=[(194, 186, 16), (226, 216, 11)]))
 
     def update(self):
         self.screen.fill('black')
@@ -160,6 +233,7 @@ class MainEditor:
         self.map.update()
 
         [but.update() for but in self.buttons]
+        [but.update() for but in self.sbuttons]
 
         pygame.display.flip()
 
@@ -173,6 +247,7 @@ class MainEditor:
 
             elif event.type == pygame.MOUSEBUTTONDOWN:
                 [but.mouseClick() for but in self.buttons]
+                [but.mouseClick() for but in self.sbuttons]
 
                 self.map.mouseClick(self.mouseX, self.mouseY)
 
@@ -183,17 +258,22 @@ class MainEditor:
                 else:
                     self.movekeys[event.key] = True
 
+                    if event.key == pygame.K_e:
+                        self.map.zoom += 3
+                    elif event.key == pygame.K_q:
+                        self.map.zoom -= 3
+
             elif event.type == pygame.KEYUP:
                 self.movekeys[event.key] = False
 
         if self.movekeys[pygame.K_w]:
-            self.map.Yoffset += 0.01
+            self.map.Yoffset += 0.02
         elif self.movekeys[pygame.K_s]:
-            self.map.Yoffset -= 0.01
+            self.map.Yoffset -= 0.02
         elif self.movekeys[pygame.K_a]:
-            self.map.Xoffset += 0.01
+            self.map.Xoffset += 0.02
         elif self.movekeys[pygame.K_d]:
-            self.map.Xoffset -= 0.01
+            self.map.Xoffset -= 0.02
 
     def run(self):
         while True:
